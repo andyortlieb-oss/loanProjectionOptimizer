ca8bc318ea024d4e190080b7239a0f6078ab9119
Ortlieb <andy.ortlieb@it12-0297.opasc.net>
2012-10-14 18:40:09 -0500

    Crude way of delivering all excess funds to largest interest creator

--- pub/loanui.js
+++ pub/loanui.js
@@ -33,7 +33,7 @@ function recalculate(){
 	var newrow;
 	var html;
 	var reportDateTrack = new Date();
-	var myloan, myprincipal, myinterest;
+	var myloan, myprincipal;
 
 	var fundsAvailable = parseFloat(fundsavail.value);
 	var tmpFunds=0;
@@ -41,6 +41,9 @@ function recalculate(){
 
 	var orPaid = 0;
 	var mrPrincipal=0, mrCurrInterest=0, mrTotalInterest=0, mrPaid=0;
+
+	var optimization = $("#optimization")[0].value; 
+
 
 
 	loanprojections.innerHTML="";
@@ -68,6 +71,7 @@ function recalculate(){
 		// Make Minimum payments
 		for (var i=0; i < loans.length; ++i){
 			myloan = loans[i];
+			myloan.stashinterest = 0;
 
 			if (count===1) { // Reset our nonsense.
 				myloan.total = 0;
@@ -91,17 +95,17 @@ function recalculate(){
 				myprincipal = myloan.stashprincipal;
 
 
-				myinterest = ( myprincipal * (myloan.apr/100) ) / 12;
-				myinterest = roundup( myinterest );
-				myloan.total+=myinterest;
+				myloan.stashinterest = ( myprincipal * (myloan.apr/100) ) / 12;
+				myloan.stashinterest = roundup( myloan.stashinterest );
+				myloan.total+=myloan.stashinterest;
 				myloan.total = roundup( myloan.total );
 
-				tmpPmt = Math.min(parseFloat(myloan.payment), tmpFunds, parseFloat(myloan.principal)+myinterest);
+				tmpPmt = Math.min(parseFloat(myloan.payment), tmpFunds, parseFloat(myloan.principal)+myloan.stashinterest);
 				tmpFunds = tmpFunds - tmpPmt;
 				orPaid += tmpPmt;
 				mrPaid += tmpPmt;
 
-				myloan.stashprincipal = parseFloat(myprincipal) + parseFloat(myinterest) - parseFloat(tmpPmt);
+				myloan.stashprincipal = parseFloat(myprincipal) + parseFloat(myloan.stashinterest) - parseFloat(tmpPmt);
 				myloan.stashprincipal = roundup( myloan.stashprincipal );
 				if (myloan.stashprincipal > myprincipal){
 					myloan.incline++;
@@ -117,7 +121,7 @@ function recalculate(){
 				html += "<td>"+myprincipal+"</td>"; mrPrincipal += parseFloat(myprincipal);
 				html += "<td>"+myloan.apr+"</td>";
 				html += "<td>"+tmpPmt+"</td>";
-				html += "<td>"+myinterest+"</td>"; mrCurrInterest += parseFloat(myinterest);
+				html += "<td>"+myloan.stashinterest+"</td>"; mrCurrInterest += parseFloat(myloan.stashinterest);
 				html += "<td>"+myloan.total+"</td>";
 
 				newrow.innerHTML = html;
@@ -136,6 +140,40 @@ function recalculate(){
 
 
 		// Allocate leftover funds for extra paments
+		if (tmpFunds > 0 && optimization!=='manual'){
+			switch (optimization){
+				case "allToHighestMonthlyInterest":
+					//while( tmpFunds ){
+
+
+						console.log("A")
+						// Find the highest current interest.
+						var winner = loans.sort(function(a,b){return (b.stashinterest||0)-(a.stashinterest||0)})[0];
+						console.log(winner, winner.stashinterest);
+
+						tmpPmt = Math.min(winner.stashprincipal, tmpFunds);
+						tmpFunds -= tmpPmt;
+						winner.stashprincipal -= tmpFunds;
+						orPaid += tmpFunds;
+
+						var extranewrow = document.createElement('tr');
+						extranewrow.className = 'extraPmt';
+
+						html = "<td>"+winner.name+"</td>";
+						html += "<td></td>";
+						html += "<td></td>";
+						html += "<td>"+tmpPmt+"</td>";
+						html += "<td></td>";
+						html += "<td></td>";
+						extranewrow.innerHTML = html;
+
+						loanprojections.appendChild(extranewrow);
+
+					//}				
+				break;
+
+			}
+		}
 		// for (var i=0; i < loans.length; ++i){
 		// 	myloan = loans[i];
 		// }
@@ -146,7 +184,7 @@ function recalculate(){
 		///////////////////////////////////
 		newrow = document.createElement('tr');
 		newrow.className = 'monthlyTotal';
-		newrow.innerHTML = "<td>("+tmpFunds+" leftover funds)</td>";
+		newrow.innerHTML = "<td>("+roundup(tmpFunds)+" leftover funds)</td>";
 		newrow.innerHTML += "<td>"+roundup(mrPrincipal)+"</td>"
 		newrow.innerHTML += "<td></td>";
 		newrow.innerHTML += "<td>"+mrPaid+"</td>";
@@ -193,8 +231,8 @@ function addloan(config){
 	html += "<td><input type='text' id='l"+id+"_principal' onchange='update("+id+",\"principal\");'  /></td>";
 	html += "<td><input type='text' id='l"+id+"_apr' onchange='update("+id+",\"apr\");' /></td>";
 	html += "<td><input type='text' id='l"+id+"_payment' onchange='update("+id+",\"payment\");' /></td>";
-	html += "<td><span id='l"+id+"_freedom'>a</span></td>";
-	html += "<td><span id='l"+id+"_total'>b</span></td>";
+	html += "<td><span id='l"+id+"_freedom'></span></td>";
+	html += "<td><span id='l"+id+"_total'></span></td>";
 
 
 	newrow.innerHTML = html;
